148b71e79d22b93d6aee181437f6591f65f2f048
Felibaba <144412720+Felibaba@users.noreply.github.com>
2025-10-21 10:34:27 +0100

    Update CORS origin URLs in server.js

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ process.on('SIGTERM', () => {
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://plexzora.onrender.com', 'https://smavot.onrender.com/login.html'],
+  origin: ['http://localhost:3000', 'https://plexzoran.onrender.com', 'https://smavon.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
   credentials: false,
